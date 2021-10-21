67c714ad44bb3c4465deb9c12824f89ba9b4fddf
ChernoBen <bneto@computacao.cua.ufmt.br>
2021-10-21 16:10:09 -0400

    server streaming greet many times

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const protoLoader = require("@grpc/proto-loader");
 const grpc = require("grpc");
+const { clearInterval } = require("timers");
 
 //grpc service definition for greet
 
@@ -13,16 +14,35 @@ const greetProtoDefinition = protoLoader.loadSync(greetProtoPath,{
     eneofs:true
 });
 const greetPackageDefinition = grpc.loadPackageDefinition(greetProtoDefinition).greet;
-//
+//unary
 function greet(call,callback){
     var firstName = call.request.greeting.first_name;
     var lastName = call.request.greeting.last_name;
     callback(null,{result:"hello "+firstName+" "+lastName});
 }
+
+//server streaming
+function greetManyTimes(call,callback){
+    var firstName = call.request.getGreeting().getFirstName();
+    var greetManyTimesResponse = new greets.GreetManyTimesResponse();
+    let count = 0,intervalID = setInterval(function(){
+        greetManyTimesResponse.setResul(firstName);    
+        //setup streaming
+        call.write(greetManyTimesResponse);
+        if(++count > 9){
+            clearInterval(intervalID);
+            //fechar apos enviar todas as mensagens necessárias
+            call.end();
+        }
+        
+    },1000); 
+}
+
 function main(){
     const server = new grpc.Server()
     server.addService(greetPackageDefinition.GreetService.service,{
-        greet:greet
+        greet:greet,
+        greetManyTimes:greetManyTimes
     });
     server.bind("127.0.0.1:50051",grpc.ServerCredentials.createInsecure());
     server.start();
